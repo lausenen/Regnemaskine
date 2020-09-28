2ec2e7639d4169baf96ac19f08fc5570714f3c05
lau.thomsen.privat@gmail.com <lau.thomsen.privat@gmail.com>
2020-09-28 14:34:11 +0200

    Nutty

--- Myndighedstjekkeren.java
+++ Myndighedstjekkeren.java
@@ -2,7 +2,7 @@ public class Myndighedstjekkeren {
     public static void main(String[] args) {
         java.util.Scanner brugerinput = new java.util.Scanner(System.in);
         int alder = brugerinput.nextInt();
-    if (alder >= 18) {
+    if (alder >= 21) {
         System.out.println("Du er myndig");
     }
     //suck my fat chubby nuts
